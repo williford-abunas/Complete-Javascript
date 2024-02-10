efdf8d2bd686822f026492050557617d623b701d
williford abunas <abunaswilliford.com>
2024-02-10 22:25:32 +1300

    check input if number

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const hiddenNum = document.querySelector('.number');
+const again = document.querySelector('.again');
+const input = document.querySelector('.guess');
+const btn = document.querySelector('.check');
+const message = document.querySelector('.message');
+const score = document.querySelector('.score');
+const highScore = document.querySelector('.highscore');
+
+btn.addEventListener('click', checkInput);
+
+function checkInput() {
+  const guess = Number(input.value);
+
+  if (!guess) {
+    message.textContent = '⛔ Not a number!';
+  }
+}
