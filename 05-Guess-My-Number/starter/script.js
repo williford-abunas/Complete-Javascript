1b31cd8a0db7e10c4d4fc30b34e1ab3a31abed9c
williford abunas <abunaswilliford.com>
2024-02-11 20:31:53 +1300

    game logic

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -5,8 +5,14 @@ const again = document.querySelector('.again');
 const input = document.querySelector('.guess');
 const btn = document.querySelector('.check');
 const message = document.querySelector('.message');
-const score = document.querySelector('.score');
+const scoreEl = document.querySelector('.score');
 const highScore = document.querySelector('.highscore');
+
+const numberToGuess = Math.trunc(Math.random() * 20) + 1;
+
+let score = 20;
+
+hiddenNum.textContent = numberToGuess;
 
 btn.addEventListener('click', checkInput);
 
@@ -15,5 +21,25 @@ function checkInput() {
 
   if (!guess) {
     message.textContent = '⛔ Not a number!';
+  } else if (guess === numberToGuess) {
+    message.textContent = '🎉 Correct Number!';
+  } else if (guess > numberToGuess) {
+    if (score > 1) {
+      message.textContent = '⬆️ Too High!';
+      score--;
+      scoreEl.textContent = score;
+    } else {
+      message.textContent = '💥 You lost the game!';
+      scoreEl.textContent = 0;
+    }
+  } else if (guess < numberToGuess) {
+    if (score > 1) {
+      message.textContent = '⬆️ Too Low!';
+      score--;
+      scoreEl.textContent = score;
+    } else {
+      message.textContent = '💥 You lost the game!';
+      scoreEl.textContent = 0;
+    }
   }
 }
