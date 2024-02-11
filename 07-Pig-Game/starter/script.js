2bb118334a89305300b3ec3eb9a0a4d32565c911
williford abunas <abunaswilliford.com>
2024-02-12 00:57:41 +1300

    player 0 score display

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,31 @@
 'use strict';
+
+const score0El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+let currentScore = 0;
+
+//Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+//Rolling dice
+btnRoll.addEventListener('click', function () {
+  const randomNum = Math.trunc(Math.random() * 6) + 1;
+
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${randomNum}.png`;
+
+  if (randomNum !== 1) {
+    currentScore += randomNum;
+    current0El.textContent = currentScore;
+  } else {
+  }
+});
