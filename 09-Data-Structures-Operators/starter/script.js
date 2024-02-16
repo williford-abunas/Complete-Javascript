613d5c8cd21220eb39e7c2e0d92e663eac9158fa
williford abunas <abunaswilliford.com>
2024-02-16 15:42:30 +1300

    optional chaining, lopping

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -12,6 +12,10 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order(num1, num2) {
+    return `here is your order ${num1} ${num2}`;
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -27,3 +31,51 @@ const restaurant = {
     },
   },
 };
+
+// if (restaurant.openingHours && restaurant.openingHours.mon)
+//   console.log(restaurant.openingHours.mon.open);
+
+//OPtional chaining
+
+console.log(restaurant.openingHours.mon?.open);
+
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+for (const day of days) {
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`${open} on ${day}`);
+}
+
+// MEthods
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exsit');
+
+//arrays
+const users = [{ name: 'Will', email: 'hello@will.com' }];
+console.log(users[0]?.name ?? 'User array empty');
+
+//LOOPING
+
+//property names
+const { openingHours } = restaurant;
+const properties = Object.keys(openingHours);
+console.log(properties);
+
+let openStr = `We are open on ${properties.length} days:`;
+console.log(openStr);
+
+for (let i = 0; i < properties.length; i++) {
+  const day = properties[i];
+  openStr += ` ${day}${i === properties.length - 1 ? '.' : ','} `;
+}
+console.log(openStr);
+
+//Property values
+
+const values = Object.values(openingHours);
+console.log(values);
+
+const entries = Object.entries(openingHours);
+console.log(entries);
+
+for (const [key, { open, close }] of entries) {
+  console.log(`On ${key} we open at ${open} and close at ${close}`);
+}
