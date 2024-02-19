f2bddd245e6b09ed2a08817feeefddd3ede3ffe9
williford abunas <abunaswilliford.com>
2024-02-20 12:54:46 +1300

    find method

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -211,14 +211,21 @@ console.log(maxNum);
 //   return averageAdult;
 // };
 
-const calcAverageHumanAge = function (ages) {
-  const humanAges = ages
+// const calcAverageHumanAge = function (ages) {
+//   const humanAges = ages
+//     .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+//     .filter(age => age > 18)
+//     .reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
+
+//   return humanAges;
+// };
+
+//ARROW
+const calcAverageHumanAge = ages =>
+  ages
     .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
     .filter(age => age > 18)
     .reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
-
-  return humanAges;
-};
 
 console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
 
@@ -230,3 +237,19 @@ const totalDepositInUsd = movements
   .reduce((acc, cur) => acc + cur);
 
 console.log(totalDepositInUsd);
+
+//FIND - retrieve one element base on condition
+const firstWithdraw = movements.find(mov => mov < 0);
+console.log(firstWithdraw);
+
+const account = accounts.find(acc => acc.owner === 'Jessica Davis');
+console.log(account);
+
+const accountJess = function () {
+  for (const acc of accounts) {
+    if (acc.owner === 'Jessica Davis') {
+      return acc;
+    }
+  }
+};
+console.log(accountJess());
