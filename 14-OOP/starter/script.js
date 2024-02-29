0d978e9bd15745de843f7358fac0f86fe70b0c62
williford-abunas <abunaswilliford@gmail.com>
2024-02-29 17:14:03 +1300

    occ part

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -21,3 +21,99 @@ console.log(bob, jill);
 
 console.log(will instanceof Person);
 //bad practice to include methods inside constructor - result will be multiple copies of fn per instance
+
+//Encapsulation
+class BankAccount {
+  constructor(accountNumber, balance) {
+    this.accountNumber = accountNumber;
+    this.balance = balance;
+  }
+
+  deposit(amount) {
+    this.balance += amount;
+  }
+
+  withdrawal(amount) {
+    if (this.balance >= amount) {
+      this.balance -= amount;
+    } else {
+      console.log('Insufficient balance!');
+    }
+  }
+
+  getAccountInfo() {
+    return this;
+  }
+}
+
+BankAccount.prototype;
+const acct1 = new BankAccount(12345, 5000);
+const acct2 = new BankAccount(54321, 3000);
+console.log(acct1);
+console.log(acct1.getAccountInfo());
+
+acct1.deposit(500);
+console.log(acct1);
+acct2.withdrawal(6000);
+console.log(acct2);
+//Inheritance
+class Animal {
+  constructor(name) {
+    this.name = name;
+  }
+
+  eat() {
+    console.log(`${this.name} is eating`);
+  }
+}
+
+class Dog extends Animal {
+  bark() {
+    console.log('Woof woof!');
+  }
+}
+
+class Cat extends Animal {
+  meow() {
+    console.log('Meow meow!');
+  }
+}
+
+const myDog = new Dog('Kobe');
+console.log(myDog);
+myDog.eat();
+myDog.bark();
+
+const myCat = new Cat('Joy');
+myCat.eat();
+myCat.meow();
+
+//Polymorphism
+class Human extends Animal {
+  eat() {
+    return super.eat() + ' ' + 'polymorphed method';
+  }
+}
+
+const rey = new Human('Rey');
+console.log(rey.eat());
+
+//Abstraction
+class Vehicle {
+  constructor(make, model) {
+    this.make = make;
+    this.model = model;
+  }
+
+  start() {
+    return `${this.make} ${this.model} is starting`;
+  }
+
+  stop() {
+    return `${this.make} ${this.model} is stopping`;
+  }
+}
+
+const myCar = new Vehicle('Toyota', 'Camry');
+console.log(myCar.start());
+console.log(myCar.stop());
